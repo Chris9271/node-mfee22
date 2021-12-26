450c6092c7e391883c0eb03117f209138b9d45e7
ChrisWeng <chrisweng71@outlook.com>
2021-12-26 14:27:39 +0800

    [UPDATE] sum.js

--- basic/sum.js
+++ basic/sum.js
@@ -1,8 +1,10 @@
 function sum(n){
+    if(n < 1){
+        return 0;
+    }
     result = (1 + n) * n / 2;
-    return result;
 }
-console.log(sum(1));
+console.log(sum(1)); 1
 console.log(sum(2));
 console.log(sum(5));
 console.log(sum(10));
