b8089c8401a4d0f6e861bc5d7244e94150fccd5e
ChrisWeng <chrisweng71@outlook.com>
2021-12-26 14:37:21 +0800

    [UPDATE] add return

--- basic/sum.js
+++ basic/sum.js
@@ -1,8 +1,20 @@
 function sum(n){
+    // let result = 0;
+    // for (let i = 0; i <= n; i++){
+    //     result += i;
+    // }
+    // return result;
+
     if(n < 1){
         return 0;
     }
-    result = (1 + n) * n / 2;
+    return result = (1 + n) * n / 2;
+
+    // recursive - 遞迴
+    // if(n < 1){
+    //     return 0;
+    // }
+    // return sum(n - 1) + n;
 }
 console.log(sum(1)); 1
 console.log(sum(2));
