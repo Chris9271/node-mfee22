7cb8cea325f6ac6fdf820aa1bfa91c763d84d74a
ChrisWeng <chrisweng71@outlook.com>
2022-01-09 10:20:45 +0800

    [UPDATE] IIFE ver. and add try..catch

--- callback/readfile3.js
+++ callback/readfile3.js
@@ -5,7 +5,9 @@ const {readFile} = require("fs");
 // new Promise( executor = (resolve, reject) => { ... } );
 
 // 建立promise構造函數
-const readFilePromise = new Promise((resolve, reject) => {
+function readFilePromise(){
+    return new Promise((resolve, reject) => {
+// const readFilePromise = new Promise((resolve, reject) => {
     // 立即執行，如函數有err則reject 否則就回傳data(resolve)
         readFile("./test.txt", "utf-8", (err, data) => {
             if(err){
@@ -15,10 +17,25 @@ const readFilePromise = new Promise((resolve, reject) => {
             }
         })
     })
-
-async function printFile(){
-    let show = await readFilePromise;
-    console.log(show);
 }
 
-printFile();
+// async function printFile(){
+//     try{
+//         let show = await readFilePromise;
+//         console.log(show);
+//     }catch(err){
+//         console.error(err);
+//     }
+// }
+
+// async await 需利用 try...catch block來抓error
+(async () => {
+    try{
+        let show = await readFilePromise();
+        console.log(show);
+    }catch(err){
+        console.error(err);
+    }
+})();
+
+// printFile();
