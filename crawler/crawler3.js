56cacf32e5294645fc30371f63f2be50a05a49aa
ChrisWeng <chrisweng71@outlook.com>
2022-01-15 12:43:31 +0800

    [UPDATE]今天日期爬蟲取得月份方式改寫

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -20,47 +20,50 @@ const {readFile} = require("fs/promises");
             // let date = "20220111";
             let standardDate = new Date();
             let year = standardDate.getFullYear().toString();
-            let month = standardDate.getMonth();
-            switch(month){
-                case 0:
-                    month = "01";
-                    break;
-                case 1:
-                    month = "02";
-                    break;
-                case 2:
-                    month = "03";
-                    break;
-                case 3:
-                    month = "04";
-                    break;
-                case 4:
-                    month = "05";
-                    break;
-                case 5:
-                    month = "06";
-                    break;
-                case 6:
-                    month = "07";
-                    break;
-                case 7:
-                    month = "08";
-                    break;
-                case 8:
-                    month = "09";
-                    break;
-                case 9:
-                    month = "10";
-                    break;
-                case 10:
-                    month = "11";
-                    break;
-                case 11:
-                    month = "12";
-                    break;
-                default:
-                    break;
+            let month = standardDate.getMonth() + 1;
+            if(month < 10){
+                month = "0" + month;
             }
+            // switch(month){
+            //     case 0:
+            //         month = "01";
+            //         break;
+            //     case 1:
+            //         month = "02";
+            //         break;
+            //     case 2:
+            //         month = "03";
+            //         break;
+            //     case 3:
+            //         month = "04";
+            //         break;
+            //     case 4:
+            //         month = "05";
+            //         break;
+            //     case 5:
+            //         month = "06";
+            //         break;
+            //     case 6:
+            //         month = "07";
+            //         break;
+            //     case 7:
+            //         month = "08";
+            //         break;
+            //     case 8:
+            //         month = "09";
+            //         break;
+            //     case 9:
+            //         month = "10";
+            //         break;
+            //     case 10:
+            //         month = "11";
+            //         break;
+            //     case 11:
+            //         month = "12";
+            //         break;
+            //     default:
+            //         break;
+            // }
             let date = standardDate.getDate().toString();
             let searchDate = year+month+date;
             let stockNum = await readFile("./stock.txt", "utf-8");
